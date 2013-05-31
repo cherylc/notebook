664fcbb66d0684328e7f7b260ad226f5af364d77
Jeff Cohen <jeff@starterleague.com>
2013-05-31 15:52:24 -0500

    Add a form for adding new items

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -16,7 +16,7 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.json { render json: @note }
     end
   end
