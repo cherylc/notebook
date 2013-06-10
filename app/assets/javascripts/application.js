b3ebe291c43c9786572722d03cb2ac2846f9910b
Raghu Betina <rbetina@gmail.com>
2013-06-10 16:52:16 -0500

    Added droppable trash area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,24 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require private_pub
 //= require_tree .
 
+$(document).ready(function() {
+  $(".note").draggable();
+
+  $("#trash").droppable({
+    drop: function(event, ui) {
+      $(ui.draggable).fadeOut();
+      $.ajax({
+        url: "/notes/" + ui.draggable.data("nid"),
+        type: "delete"
+      })
+    }
+  });
+});
 
 // $(document).ready(function() {
 //   $("li").on("click", function(e) {
